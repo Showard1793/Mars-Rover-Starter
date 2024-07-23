79cc068c1e0098ee347ff417e9b97a11ca079788
Stephen Howard <Showard1793@gmail.com>
2024-07-23 09:21:35 -0500

    Final

--- command.js
+++ command.js
@@ -1,10 +1,10 @@
 class Command {
    constructor(commandType, value) {
-     this.commandType = commandType;
+     this.commandType = commandType; //Test 2 ("constructor sets command type")
      if (!commandType) {
-       throw Error("Command type required.");
+       throw Error("Command type required."); //Test 1 ("throws error if command type is NOT passed into constructor as the first parameter")
      }
-     this.value = value;
+     this.value = value; //Test 3 ("constructor sets a value passed in as the 2nd argument")
    }
  }
  
--- message.js
+++ message.js
@@ -3,10 +3,10 @@ class Message {
    
       constructor(name, commands) {
           if (!name) {
-              throw Error("Message name required.");
+              throw Error("Message name required."); //Test 4 ("throws error if a name is NOT passed into the constructor as the first parameter")
           }
-          this.name = name;
-          this.commands = commands;
+          this.name = name; //Test 5 ("constructor sets name")
+          this.commands = commands; //Test 6 ("contains a commands array passed into the constructor as the 2nd argument")
       }
   }
 
